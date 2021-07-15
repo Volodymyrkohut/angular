db0c1eee6e62007a0b2e51c2b70bf475aaf2ae0d
Volodymyr Kohut <volodymyrkohut1@gmail.com>
2021-07-15 16:38:24 +0300

    mi added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular';
+  data = {};
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(){
+    this.http.get('https://api.dev24.bukovel.net/b24/desktop/hotels').subscribe((response)=>{
+      console.log("response",response);
+      this.data = response
+    })
+    console.log("heree")
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { NoFoundComponent } from './components/no-found/no-found.component';
-import { HotelsComponent } from './components/hotels/hotels.component';
-import { HotelDetailComponent } from './components/hotel-detail/hotel-detail.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {NoFoundComponent} from './components/no-found/no-found.component';
+import {HotelsComponent} from './components/hotels/hotels.component';
+import {HotelDetailComponent} from './components/hotel-detail/hotel-detail.component';
+import {ProfileComponent} from './components/profile/profile.component';
 import {FormsModule} from "@angular/forms";
 import {HotelRoomItemComponent} from "./components/hotel-detail/hotel-room-item/hotel-room-item.component";
 import {HotelRoomListComponent} from "./components/hotel-detail/hotel-room-list/hotel-room-list.component";
@@ -16,6 +16,37 @@ import {HotelTestComponent} from "./components/hotel-detail/hotel-test/hotel-tes
 import {StyleDirective} from "./directives/style.directive";
 import {IfnotDirective} from "./directives/ifnot.directive";
 import {MultyPipe} from "./pipes/multy.pipe";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {HeaderComponent} from "./components/layout/header/header.component";
+import {LayoutComponent} from "./components/layout/layout/layout.component";
+import {SidebarComponent} from "./components/layout/sidebar/sidebar.component";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+
+const MatModules = [
+  MatSliderModule,
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatMenuModule,
+  MatCardModule,
+  MatSlideToggleModule
+];
+
+const LayoutModules = [
+  HeaderComponent,
+  LayoutComponent,
+  SidebarComponent,
+]
 
 @NgModule({
   declarations: [
@@ -32,16 +63,22 @@ import {MultyPipe} from "./pipes/multy.pipe";
     StyleDirective,
     IfnotDirective,
     MultyPipe,
+
+    ...LayoutModules,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    ...MatModules,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
-    // CommonModule,
+export class AppModule {
+}
+
+// CommonModule,
 // import {CommonModule} from "@angular/common";
